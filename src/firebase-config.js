1f9c5d7afd5b0bea5be924f525af8dadd406e33c
AnikHaque <ekramul15-811@diu.edu.bd>
2022-09-27 18:24:34 +0600

    added

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,13 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: <YOURAPIKEY>,
-  authDomain: <YOURAUTHDOAMIN>,
-  projectId: <YOURPROJECTID>,
-  storageBucket: <YOURSTORAGEBUCKET>,
-  messagingSenderId: <YOURMESSAGINGSENDERID>,
-  appId: <YOURAPPID>,
-  measurementId: <YOURMEASUREMENTID>,
+ 
 };
 
 const app = initializeApp(firebaseConfig);
